24c4dc1e7450ea18f267443661eeac9fc6b21fd1
pranayjoshi <pranayjoshi4466@gmail.com>
2023-03-31 16:37:04 +0530

    Friend Added

---
+++ client/src/components/Friend.tsx
@@ -0,0 +1,66 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "state";
+import UserImage from "./UserImage";
+import {UserPlus, UserMinus} from "@styled-icons/heroicons-outline";
+
+interface Props{
+    friendId: string, name:string, subtitle:string, userPicturePath:string
+}
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }:Props) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state:any) => state.user);
+  const token = useSelector((state:any) => state.token);
+  const friends = useSelector((state:any) => state.user.friends);
+
+  const isFriend = friends.find((friend:any) => friend._id === friendId);
+
+  const patchFriend = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${_id}/${friendId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  return (
+    <div className="flex">
+      <div className="flex">
+        <UserImage image={userPicturePath} size="55px" />
+        <div
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <p
+            className="text-gray-700 dark:text-gray-200 cursor-pointer"
+          >
+            {name}
+          </p>
+          <p className="text-gray-400">
+            {subtitle}
+          </p>
+        </div>
+      </div>
+      <button className="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => patchFriend()}>
+        {isFriend ? (
+          <UserMinus className="text-gray-700 dark:text-gray-200" />
+        ) : (
+          <UserMinus className="text-gray-700 dark:text-gray-200" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Friend;
---
+++ client/src/widgets/FriendListWidget.tsx
@@ -0,0 +1,53 @@
+import Friend from "components/Friend";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+
+interface Props {
+    userId: string
+}
+
+const FriendListWidget = ({ userId }:Props) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state:any) => state.token);
+  const friends = useSelector((state:any) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div >
+      <p
+        className="text-gray-700 dark:text-gray-200"
+      >
+        Friend List
+      </p>
+      <div className="divide-y flex flex-col">
+        {friends.map((friend:any) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FriendListWidget;
