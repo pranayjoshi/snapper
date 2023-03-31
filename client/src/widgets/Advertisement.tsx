76250351dfa1494de6c43266cd8b76b59d0bb4aa
pranayjoshi <pranayjoshi4466@gmail.com>
2023-03-31 08:35:39 +0530

    added Advertisement Widget

--- client/src/widgets/Advertisement.tsx
+++ client/src/widgets/Advertisement.tsx
@@ -8,7 +8,7 @@ const Data = [
       "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     tags: ["photography", "travel", "winter"],
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. .",
   },
 ];
 
@@ -20,24 +20,25 @@ export const Advertisement = () => {
           key={item.id}
           className="rounded-lg overflow-hidden shadow-lg dark:shadow-gray-800 border"
         >
-            <div className="px-6 py-4 flex justify-items-end">
-            <div className="text-gray-700 text-xl mb-2 dark:text-gray-200">Sponsered</div>
-            <p className="text-gray-500  text-base">
+            <div className="px-6 pt-4 flow-root">
+            <div className="text-gray-700 float-left text-base dark:text-gray-200">Sponsered</div>
+            <p className="text-gray-500 float-right text-xs pt-1">
               Create add
             </p>
           </div>
-          <img className="w-full rounded-2xl" src={item.image} alt="Mountain" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2 dark:text-gray-200">{item.title}</div>
-            <p className="text-gray-700 dark:text-gray-300 text-base">
+          <div className="m-4"><img className="rounded-xl" src={item.image} alt="Mountain" /></div>
+          
+          <div className="px-6">
+            <div className="text-gray-700 font-bold text-base mb-2 dark:text-gray-200">{item.title}</div>
+            <p className="text-gray-500 dark:text-gray-400 text-xs">
               {item.description.substring(0, 100)}
             </p>
           </div>
-          <div className="px-6 pt-4 pb-2">
+          <div className="px-6 pt-2 pb-2">
             {item.tags.map((tag) => (
               <span
                 key={tag}
-                className="inline-block bg-gray-200 dark:bg-gray-800 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2"
+                className="inline-block bg-gray-200 dark:bg-gray-800 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2"
               >
                 {tag}
               </span>
