3d7b228ffd3cc7aa648b4803b712b924916c60f0
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-01 07:35:05 +0530

    Postswidget added

--- client/src/widgets/PostsWidget.tsx
+++ client/src/widgets/PostsWidget.tsx
@@ -21,6 +21,7 @@ const PostsWidget = ({ userId, isProfile = false }:Props) => {
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
+    console.log(data)
     dispatch(setPosts({ posts: data }));
   };
 
