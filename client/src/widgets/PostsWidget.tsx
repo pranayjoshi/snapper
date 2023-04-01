6d441e0eead7e0fe8fed610c14c23e05d620b8cd
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-01 06:14:12 +0530

    Postswidget added

---
+++ client/src/widgets/PostsWidget.tsx
@@ -0,0 +1,93 @@
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+
+interface Props{
+    userId:string,
+    isProfile:boolean
+}
+
+const PostsWidget = ({ userId, isProfile = false }:Props) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state:any) => state.posts);
+  const token = useSelector((state:any) => state.token);
+
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  interface Props2{
+    _id:string,
+    userId:string,
+    firstName:string,
+    lastName:string,
+    description:string,
+    location:string,
+    picturePath:string,
+    userPicturePath:string,
+    likes:any,
+    comments:Array<String>,
+  }
+
+  return (
+    <>
+      {posts.map(
+        ({
+            _id,
+            userId,
+            firstName,
+            lastName,
+            description,
+            location,
+            picturePath,
+            userPicturePath,
+            likes,
+            comments,
+        }:Props2) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
+};
+
+export default PostsWidget;
